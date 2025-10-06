d266015064aa1a6890119c1a91f127dc19194c9e
Azalea-u <salmi.mouad137@gmail.com>
2025-10-06 17:14:42 +0100

    working on cd command

--- src/behavior/shell.rs
+++ src/behavior/shell.rs
@@ -4,12 +4,16 @@ use std::env;
 
 pub struct Shell {
     pub cwd: PathBuf,
+    pub old_cwd: Option<PathBuf>,
 }
 
 impl Shell {
     pub fn new() -> Self {
         let cwd = env::current_dir().unwrap_or_else(|_| PathBuf::from("/"));
-        Self { cwd }
+        Self {
+            cwd,
+            old_cwd: None,
+        }
     }
 
     pub fn prompt(&self) -> String {
@@ -20,5 +24,11 @@ impl Shell {
         if let Ok(current) = env::current_dir() {
             self.cwd = current;
         }
+    }
+
+    pub fn update_dir(&mut self, new_dir: PathBuf) {
+        self.old_cwd = Some(self.cwd.clone());
+        self.cwd = new_dir.clone();
+        let _ = env::set_current_dir(&new_dir);
     }
 }
